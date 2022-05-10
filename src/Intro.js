6fd40e7b3768ffd1dcb22a370ca6a45f4f3bfb77
Simon Kassab <simkassab@gmail.com>
2022-05-09 20:20:21 -0400

    Basic horizontal scrolling

---
+++ src/Intro.js
@@ -0,0 +1,11 @@
+import './Intro.css';
+
+function Intro() {
+  return (
+    <div className="Intro">
+
+    </div>
+  );
+}
+
+export default Intro;
---
+++ src/Landing.js
@@ -0,0 +1,11 @@
+import './Landing.css';
+
+function Landing() {
+  return (
+    <div className="Landing">
+
+    </div>
+  );
+}
+
+export default Landing;
