dc579cfe57f32de655382ecaff8f6adf5b28a003
Simon Kassab <simkassab@gmail.com>
2022-05-19 18:49:39 -0400

    Finished Landing

--- src/Landing.js
+++ src/Landing.js
@@ -3,41 +3,24 @@ import './Landing.css';
 function Landing() {
   return (
     <div className='Landing'>
-      {/* <div className='landing-photo-section'>
-        <div className='landing-photo'>
-
-        </div>
-        <div className='landing-photo-text'>
-          <p>Home located in Lac-Brome, Quebec - Designed by Thomas Balaban</p>
-        </div>
-      </div>
-      <div className='landing-text'>
-        <div className='landing-text-title'>
-          <h1>Quebec</h1>
-          <h1>Housing Market</h1>
-          <h1>Analysis</h1>
-        </div>
-        <div className='landing-text-text'>
-          <p>By designing a web scraper, I was able to gather information on over 100k sold homes from the past 15 years in Quebec.
-            I then used visualization tools to generate a series of graphs offering a unique insight into Quebec's housing market.
-            Finally, I designed a neural network using Tensorflow. This allowed me to train a model capable of predicting
-            a home's final selling price with a 15% margin of error.</p>
-        </div>
-        <div className='landing-text-outro'>
-          <h2>Scroll to start.</h2>
-        </div>
-
-      </div> */}
-
       <div className='landing'>
         <div className='landing-left'>
           <div className='landing-left-photo'></div>
           <div className='landing-left-photo-description'><p>Home located in Lac-Brome, Quebec - Designed by Thomas Balaban</p></div>
         </div>
         <div className='landing-right'>
-          <div className='landing-right-title'></div>
-          <div className='landing-right-description'></div>
-          <div className='landing-right-start'></div>
+          <div className='landing-right-title'>
+            <h1>Quebec</h1>
+            <h1>Housing Market</h1>
+            <h1>Analysis</h1>
+          </div>
+          <div className='landing-right-description'>
+          <p>By designing a web scraper, I was able to gather information on over 100k sold homes from the past 15 years in Quebec.
+            I then used visualization tools to generate a series of graphs offering a unique insight into Quebec's housing market.
+            Finally, I designed a neural network using Tensorflow. This allowed me to train a model capable of predicting
+            a home's final selling price with a 15% margin of error.</p>
+          </div>
+          <div className='landing-right-start'><p>Scroll to start.</p></div>
         </div>
       </div>
     </div>
