c9baf9291da4d29a7837653dfbb5c0e4b4c9ff14
Simon Kassab <43049319+simkass@users.noreply.github.com>
2022-08-19 22:23:44 -0400

    Scraping page, first paragraph

--- src/pages/scraping/scraping.component.jsx
+++ src/pages/scraping/scraping.component.jsx
@@ -1,5 +1,21 @@
+import "./scraping.styles.scss";
+
 const Scraping = () => {
-    return <p>Scraping</p>
+  return (
+    <div className="Scraping">
+      <div className="scraping">
+        <h1>Acquiring the Data.</h1>
+        <p>
+          In Quebec, the only website (I could find) that openly exposes data on
+          sold houses is <a href="https://duproprio.com/en">DuProprio</a>. This
+          website allows homeowners to sell their houses without the use of an
+          agent. The data I was interested in was sold homes and condos,
+          anywhere in Quebec. Luckily, DuProprio allows users to filter their
+          search using these exact criteria.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Scraping;
