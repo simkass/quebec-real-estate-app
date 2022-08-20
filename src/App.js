868577ffd786cc1b58b5ee041e42b96cf5ddb014
Simon Kassab <43049319+simkass@users.noreply.github.com>
2022-08-19 22:01:28 -0400

    Start Scraping Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Home from './pages/home/home.component';
+import Scraping from './pages/scraping/scraping.component'
 
 const App = () => {
   return (
     <div className="App">
       <Home/>
+      <Scraping/>
     </div>
   );
 }
---
+++ src/pages/scraping/scraping.component.jsx
@@ -0,0 +1,5 @@
+const Scraping = () => {
+    return <p>Scraping</p>
+};
+
+export default Scraping;
